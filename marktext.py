eaf859c6f39770279305a1d754355ea583222867
Marco Konersmann <konersmann@uni-koblenz.de>
2021-02-08 09:57:29 +0100

    fixes typo in command name

--- marktext.py
+++ marktext.py
@@ -4,7 +4,7 @@ import subprocess
 import sublime
 import sublime_plugin
 
-class MarkTextCommand(sublime_plugin.WindowCommand):
+class MarktextCommand(sublime_plugin.WindowCommand):
     def run(self):
         filename = self.window.active_view().file_name()
         if filename is None:
